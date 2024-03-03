f59f1d95710107bdd2a28f5a2d7f1dd3a64fdd45
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2024-03-03 14:40:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+
+const server = new SMTPServer({
+    
+    onConnect(session, cb) {
+        // cb(new Error ('Connot accept connection'))
+
+        console.log('onConnect', session.id)
+        cb() // accept mail
+    },
+    onMailFrom(address, session, cb) {
+        console.log(`onMailFrom`, address.address, session.id)
+        cb() // accept mail
+    },
+    onRcptTo(address, session, cb) {
+        console.log(`onRcptTo`, address.address, session.id)
+        cb() // 
+    },
+    onData(stream, session, cb) {
+        stream.on('data', (data) => console.log(`onData ${data.toString()}`))
+        stream.on('end', cb)
+    },
+     
+
+});
+
+server.listen(25, () => console.log('server running on 25'))
